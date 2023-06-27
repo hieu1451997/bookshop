c17704707cb7e4b00cdbd36c48c259cc5d5392b2
hieu1451997 <hieu1451997@gmail.com>
2023-06-27 22:11:53 +0700

    khi khách hàng mua hàng insert dữ liệu vào csdl:shipping,payment,order,order details

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -16,4 +16,60 @@ class CheckoutController extends Controller
         $publisher=DB::table('tbl_publisher')->where('publisher_status','0')->orderby('publisher_id','desc')->get();
         return view('client.pages.checkout.show_checkout')->with('category_product',$category_product)->with('publisher',$publisher);
      }
+     public function login_checkout(Request $request){
+        $customer_email = $request->customer_email;
+        $customer_password = md5($request->customer_password);
+
+        $result = DB::table('tbl_customers')->where('customer_email',$customer_email)->where('customer_password',$customer_password)->first();
+
+        if ($result) {
+            Session::put('customer_name',$result->customer_name);
+            Session::put('customer_id',$result->customer_id); 
+
+            return Redirect::to('/view-checkout');
+            
+        }else {
+            Session::put('message','Email hoặc mật khẩu không đúng');
+            return Redirect::to('/view-checkout');
+        }
+     }
+   public function check_out(Request $request){
+      //Insert tbl_shipping
+      $data_shipping=array();
+      $data_shipping['customer_id']=Session::get('customer_id');
+      $data_shipping['shipping_name']=$request->shipping_name;
+      $data_shipping['shipping_email']=$request->shipping_email;
+      $data_shipping['shipping_phone']=$request->shipping_phone;
+      $data_shipping['shipping_address']=$request->shipping_address;
+      $shipping_id=DB::table('tbl_shipping')->insertGetId($data_shipping);
+      //dd($data_shipping);
+
+      //Insert tbl_payment
+      $data_payment=array();
+      $data_payment['payment_method']=$request->payment_method;
+      $data_payment['payment_status']='Đang chờ xử lý';
+      $payment_id=DB::table('tbl_payment')->insertGetId($data_payment);
+      //dd($data_payment);
+
+      //Insert tbl_order
+      $data_order=array();
+      $data_order['customer_id']=Session::get('customer_id');
+      $data_order['shipping_id']=$shipping_id;
+      $data_order['payment_id']=$payment_id;
+      $data_order['order_total']=Session('Cart')->totalPrice;
+      $data_order['order_status']='Đang chờ xử lý';
+      $order_id=DB::table('tbl_order')->insertGetId($data_order);
+      //dd($data_order);
+
+      //Insert tbl_order_details
+      foreach(Session::get('Cart')->products as $item_pro){
+         $data_order_details['order_id']=$order_id;
+         $data_order_details['product_id']=$item_pro['productInfo']->product_id;
+         $data_order_details['product_name']=$item_pro['productInfo']->product_name;
+         $data_order_details['product_price']=$item_pro['productInfo']->product_price;
+         $data_order_details['product_quantity']=$item_pro['quanty'];
+         DB::table('tbl_order_details')->insert($data_order_details);
+      }
+      echo'Thành công';
+   }
 }
--- resources/views/client/pages/checkout/show_checkout.blade.php
+++ resources/views/client/pages/checkout/show_checkout.blade.php
@@ -37,20 +37,28 @@
 							<a class="showlogin" href="#">Đăng nhập</a>
 						</div>
 						<div class="checkout_login">
-							<form class="wn__checkout__form" action="#">
+							<?php  
+								$message = Session::get('message');
+								if ($message) {
+									echo '<span class="text-alert" style="color: red;">'.$message.'</span>';
+									Session::put('message',null);
+								}
+							?>
+							<form class="wn__checkout__form" action="{{URL::to('/login-checkout')}}" method="post">
+								{{csrf_field()}}
 								<p>Nếu bạn chưa có tài khoản hãy đăng ký <a style="color: #e59285;" href="{{URL::to('/view-login-customer')}}">tại đây</a></p>
 
 								<div class="input__box">
 									<label>Email <span>*</span></label>
-									<input type="text">
+									<input type="text" name="customer_email">
 								</div>
 
 								<div class="input__box">
 									<label>Mật khẩu <span>*</span></label>
-									<input type="password">
+									<input type="password" name="customer_password">
 								</div>
 								<div class="form__btn">
-									<button>Đăng nhập</button>
+									<button type="submit">Đăng nhập</button>
 									<label class="label-for-checkbox">
 										<input id="rememberme" name="rememberme" value="forever" type="checkbox">
 										<span>Ghi nhớ đăng nhập</span>
@@ -67,62 +75,63 @@
 				</div>
 			</div>
 		@endif
-		<div class="row">
-			<div class="col-lg-6 col-12">
-				<div class="customer_details">
-					<h3>Thông tin nhận hàng</h3>
-					<div class="customar__field">
-						<div class="input_box">
-							<label>Họ và tên <span>*</span></label>
-							<input type="text">
-						</div>
-						<div class="input_box">
-							<label>Email <span>*</span></label>
-							<input type="text">
-						</div>
-						<div class="input_box">
-							<label>Số điện thoại <span>*</span></label>
-							<input type="number">
-						</div>
-						<div class="input_box">
-							<label>Địa chỉ nhận hàng <span>*</span></label>
-							<input type="text">
-						</div>
+		<form method="post" action="{{URL::to('/check-out')}}">
+			{{csrf_field()}}
+			<div class="row">			
+				<div class="col-lg-6 col-12">
+					<div class="customer_details">
+						<h3>Thông tin nhận hàng</h3>
+						<div class="customar__field">
+							<div class="input_box">
+								<label>Họ và tên <span>*</span></label>
+								<input type="text" name="shipping_name">
+							</div>
+							<div class="input_box">
+								<label>Email <span>*</span></label>
+								<input type="text" name="shipping_email">
+							</div>
+							<div class="input_box">
+								<label>Số điện thoại <span>*</span></label>
+								<input type="number"name="shipping_phone">
+							</div>
+							<div class="input_box">
+								<label>Địa chỉ nhận hàng <span>*</span></label>
+								<input type="text"name="shipping_address">
+							</div>
+						</div>					
 					</div>
-					
+					<div class="wn__accountbox">
+						<input class="input-checkbox" name="payment_method" value="1" type="checkbox">
+						<span>Thanh toán khi nhận hàng</span>
+					</div>				
 				</div>
-				
-			</div>
-			<div class="col-lg-6 col-12 md-mt-40 sm-mt-40">
-				<div class="wn__order__box">
-					<h3 class="onder__title">Sản phẩm mua</h3>
-					<ul class="order__total">
-						<li>Sản phẩm</li>
-						<li>Giá</li>
-					</ul>
-					<ul class="order_product">
-						<li>Buscipit at magna × 1<span>$48.00</span></li>
-						<li>Buscipit at magna × 1<span>$48.00</span></li>
-						<li>Buscipit at magna × 1<span>$48.00</span></li>
-						<li>Buscipit at magna × 1<span>$48.00</span></li>
-					</ul>
-					<ul class="total__amount">
-						<li>Tổng tiền <span>$223.00</span></li>
-					</ul>
+				<div class="col-lg-6 col-12 md-mt-40 sm-mt-40">
+					<div class="wn__order__box">
+						<h3 class="onder__title">Sản phẩm mua</h3>
+						<ul class="order__total">
+							<li>Sản phẩm</li>
+							<li>Giá</li>
+						</ul>
+						@if(Session::has("Cart") != null)
+	                    @foreach(Session::get('Cart')->products as $item_pro)
+							<ul class="order_product">
+								<li>{{$item_pro['productInfo']->product_name}} × {{$item_pro['quanty']}}<span>{{number_format($item_pro['price']).' VNĐ'}}</span></li>
+								
+							</ul>
+						@endforeach
+	                    
+						<ul class="total__amount">
+							<li>Tổng tiền <span>{{number_format(Session::get('Cart')->totalPrice).' VNĐ'}}</span></li>
+						</ul>
+						@endif
+					</div>
+				    
+					    <div class="check_out" style="padding-top: 30px;" >
+					        <button style="width: 50%;height: 50px;float: right;background: #d0011b;font-size: 16px;color: #fff;border: none;" type="submit">Mua hàng</button>
+					    </div>				   			   
 				</div>
-			    <div id="accordion" class="checkout_accordion mt--30" role="tablist">
-				    <div class="payment">
-				        <div class="che__header" role="tab" id="headingOne">
-				          	<a class="checkout__title" data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-				                <span>Đặt hàng</span>
-				          	</a>
-				        </div>
-				    </div>
-				   
-			    </div>
-
 			</div>
-		</div>
+		</form>
 	</div>
 </section>
 @endsection
